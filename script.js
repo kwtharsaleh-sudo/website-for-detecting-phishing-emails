58b4b622d8957586e491534215d5e61469597225
kwtharsaleh-sudo <kwtharsaleh@gmail.com>
2026-05-07 20:58:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,40 @@
-body {
-  margin: 0;
-  font-family: Arial;
-  background: white;
+function checkEmail(){
 
-  /* مهم جداً: توسيط كامل */
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+    let text = document.getElementById("emailText").value.toLowerCase();
 
-/* العنوان في النص */
-h1 {
-  margin-top: 50px;
-  font-size: 24px;
-  text-align: center;
-  color: #222;
-}
+    let fakeWords = [
+        "ربحت",
+        "اضغط هنا",
+        "كلمة المرور",
+        "تحويل أموال",
+        "bank",
+        "winner",
+        "free",
+        "click"
+    ];
 
-/* الصندوق في الوسط */
-.box {
-  margin-top: 60px;
-  background: white;
-  width: 350px;
-  padding: 30px;
-  border-radius: 15px;
+    let danger = false;
 
-  box-shadow: 0 0 15px rgba(0,0,0,0.15);
-  text-align: center;
-}
+    for(let i = 0; i < fakeWords.length; i++){
 
-/* input */
-input {
-  width: 100%;
-  padding: 12px;
-  font-size: 16px;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  margin-bottom: 10px;
-  outline: none;
-}
+        if(text.includes(fakeWords[i])){
+            danger = true;
+        }
 
-/* زر */
-button {
-  width: 100%;
-  padding: 12px;
-  background: black;
-  color: white;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-}
+    }
 
-button:hover {
-  background: #333;
-}
+    let result = document.getElementById("result");
 
-/* النتيجة */
-#result {
-  margin-top: 15px;
-  font-size: 18px;
-  font-weight: bold;
+    if(danger){
+
+        result.innerHTML = "⚠️ الإيميل خطر";
+        result.className = "danger";
+
+    }else{
+
+        result.innerHTML = "✅ الإيميل آمن";
+        result.className = "safe";
+
+    }
+
 }
