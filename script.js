b763c278ca489121d46ca2b7d5f525c9ff1493b5
kwtharsaleh-sudo <kwtharsaleh@gmail.com>
2026-05-07 19:30:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,36 @@
-const canvas = document.getElementById("particles");
-const ctx = canvas.getContext("2d");
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+  <meta charset="UTF-8">
+  <title>Email Checker Pro</title>
+  <link rel="stylesheet" href="style.css">
+</head>
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+<body>
 
-let particles = [];
+<canvas id="bg"></canvas>
 
-for (let i = 0; i < 80; i++) {
-  particles.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    r: Math.random() * 3,
-    dx: (Math.random() - 0.5),
-    dy: (Math.random() - 0.5)
-  });
-}
+<div class="container">
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  <div class="card">
 
-  ctx.fillStyle = "rgba(255,255,255,0.7)";
+    <h2>مرحباً بك </h2>
+    <p2>نظام كشف الإيميلات</p2>
 
-  particles.forEach(p => {
-    ctx.beginPath();
-    ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
-    ctx.fill();
+    <input type="text" id="email" placeholder="اكتب الإيميل هنا">
 
-    p.x += p.dx;
-    p.y += p.dy;
+    <button onclick="checkEmail()">فحص الايميل</button>
 
-    if (p.x < 0 || p.x > canvas.width) p.dx *= -1;
-    if (p.y < 0 || p.y > canvas.height) p.dy *= -1;
-  });
+    <div id="result"></div>
 
-  requestAnimationFrame(draw);
-}
+  </div>
 
-draw();
+</div>
 
-function checkEmail() {
+<!-- الصوت -->
+<audio id="safeSound" src="https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg"></audio>
+<audio id="dangerSound" src="https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg"></audio>
 
-  let email = document.getElementById("email").value;
-  let result = document.getElementById("result");
-
-  let safeSound = document.getElementById("safeSound");
-  let dangerSound = document.getElementById("dangerSound");
-
-  let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-  if (email === "") {
-    result.innerHTML = "اكتب ايميل أول";
-    result.style.color = "red";
-  }
-
-  else if (pattern.test(email)) {
-    result.innerHTML = "✔ الايميل آمن";
-    result.style.color = "green";
-
-    safeSound.play();   // صوت آمن
-  }
-
-  else {
-    result.innerHTML = "⚠ الايميل مشبوه";
-    result.style.color = "red";
-
-    dangerSound.play();  // صوت خطر
-  }
-}
+<script src="script.js"></script>
+</body>
+</html>
